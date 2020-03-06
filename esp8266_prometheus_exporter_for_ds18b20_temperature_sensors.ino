18964e96d43256356fd0d9bb8f7aaae73d878708
Patrick van Staveren <trick@vanstaveren.us>
2020-03-05 21:23:02 -0600

    ArduinoOTA support and rely on stored WiFi creds

--- esp8266_prometheus_exporter_for_ds18b20_temperature_sensors.ino
+++ esp8266_prometheus_exporter_for_ds18b20_temperature_sensors.ino
@@ -3,6 +3,7 @@
 #include <WiFiClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ArduinoOTA.h>
 
 //------------------------------------------
 //DS18B20
@@ -19,11 +20,6 @@ float tempDevF[ONE_WIRE_MAX_DEV]; //Saving the last measurement of temperature
 float tempDevFLast[ONE_WIRE_MAX_DEV]; //Previous temperature measurement
 long lastTemp; //The last measurement
 const int measureTempFrequency = 5000; //The frequency of temperature measurement
-
-//------------------------------------------
-//WIFI
-const char* ssid = "FIXME";
-const char* password = "FIXME";
 
 //------------------------------------------
 //HTTP
@@ -169,12 +165,10 @@ void MeasureTemperature(long now) {
 
 //------------------------------------------
 void setup() {
-  //Setup Serial port speed
-  Serial.begin(115200);
-
-  //Setup WIFI
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  Serial.begin(9600);
+  Serial.print("Initialized.\n\nConnecting to WiFi SSID ");
+  Serial.print(WiFi.SSID());
+  Serial.println();
 
   //Wait for WIFI connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -182,8 +176,8 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.print("Connected to network ");
+  Serial.println(WiFi.SSID());
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("MAC address: ");
@@ -197,11 +191,46 @@ void setup() {
   Serial.println("HTTP server started at ip " + WiFi.localIP().toString() );
 
   SetupDS18B20();
+
+  ArduinoOTA.setPasswordHash("f9f9c5fd6f84c7b3d2c4fb05cafcfcae");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
   long t = millis();
-
+  ArduinoOTA.handle();
   server.handleClient();
   MeasureTemperature(t);
 }
